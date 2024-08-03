625130eaa568b17f0c2ba800b0cba6c9679ef772
IlyaKrass <krasnenkov.ilya@inbox.ru>
2024-08-03 16:34:56 +0400

    Добавление файлов

---
+++ day1/01-cards/app.js
@@ -0,0 +1,21 @@
+function slidesPlugin(acliveSlide = 0) {
+    const slides = document.querySelectorAll('.slide')
+
+    slides[acliveSlide].classList.add('active')
+
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+
+            slide.classList.add('active')
+    })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
+}
+
+slidesPlugin()
